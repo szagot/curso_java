c56e18998bbbd81f14301fa21c6e44097f54db58
Daniel Bispo <szagot@gmail.com>
2020-05-03 18:32:33 -0300

    DCasses genéricas delimitadas

---
+++ classes_genericas_delimitados/src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import services.CalculationService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Uma empresa de consultoria deseja avaliar a performance de produtos,
+		 * funcionários, dentre outras coisas. Um dos cálculos que ela precisa é
+		 * encontrar o maior dentre um conjunto de elementos. Fazer um programa que leia
+		 * um conjunto de produtos a partir de um arquivo, conforme exemplo, e depois
+		 * mostre o mais caro deles.
+		 */
+
+		List<Product> list = new ArrayList<>();
+
+		String path = "c:\\eclipse\\produtos.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+			}
+			
+			// Pegando o produto mais caro
+			Product x = CalculationService.max(list);
+			System.out.println("Produto mais caro:\n" + x);
+			
+
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+
+	}
+
+}
---
+++ classes_genericas_delimitados/src/services/CalculationService.java
@@ -0,0 +1,30 @@
+package services;
+
+import java.util.List;
+
+public class CalculationService {
+
+	/**
+	 * Calcula o máximo de uma lista
+	 * Estende de Comparable para permitir o comparador de arquivos
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static <T extends Comparable<? super T>> T max(List<T> list) {
+		// A lista está vazia?
+		if (list.isEmpty()) {
+			throw new IllegalStateException("A lista não pode ser vazia");
+		}
+
+		// Procura pelo maior
+		T max = list.get(0);
+		for (T item : list) {
+			if (item.compareTo(max) > 0) {
+				max = item;
+			}
+		}
+
+		return max;
+	}
+}
