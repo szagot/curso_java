fe7776aa091cb1b76b78c6d61983ac9db5344bc5
Daniel Bispo <szagot@gmail.com>
2020-05-04 09:40:43 -0300

    Tipos curinga delimitados: implementação acertada

--- curingas_delimitados/src/application/Program.java
+++ curingas_delimitados/src/application/Program.java
@@ -21,23 +21,30 @@ public class Program {
 
 		System.out.println("Área Total: " + totalArea(myShapes));
 
-		// Se a minha lista for do tipo Círculo, haverá um erro com essa implementação,
-		// mesmo ela sendo uma implementação de Figuras
+		// Agora é permitido o tipo círculo pelo uso do caracter curinga delimitado para
+		// Shapes
 		List<Circle> myCircles = new ArrayList<>();
 		myCircles.add(new Circle(2.0));
 		myCircles.add(new Circle(3.0));
-		// O erro será gerado nessa linha abaixo comentada
-		// System.out.println("Área Total: " + totalArea(myCircles));
+		// Não tem mais erro
+		System.out.println("Área Total: " + totalArea(myCircles));
 
 	}
 
 	/**
 	 * Retorna a soma das áreas de uma figura
 	 * 
+	 * <?> Caracter curinga: aceita todo tipo de lista
+	 * 
+	 * <? extends Shape> Caracter curinga delimitado: aceita apenas tipos de lista
+	 * que implementam Shape
+	 * 
+	 * Listas de tipos curinga só podem ser acessadas, mas não alteradas
+	 * 
 	 * @param list
 	 * @return
 	 */
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
