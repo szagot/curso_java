5c5b10f5e609cb76bc30d9fdc7044b85b6524910
Daniel Bispo <szagot@gmail.com>
2020-05-07 10:23:13 -0300

    Spring Boot: Criando entidade de pedidos (Order) e associações com usuários (User)

--- projeto_spring_boot_jpa/src/main/java/br/com/zefuinha/projeto_spring_boot_jpa/entities/User.java
+++ projeto_spring_boot_jpa/src/main/java/br/com/zefuinha/projeto_spring_boot_jpa/entities/User.java
@@ -1,13 +1,18 @@
 package br.com.zefuinha.projeto_spring_boot_jpa.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user") // Opcional. Colocado apenas para manter o padrão (TOC rs)
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -71,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
