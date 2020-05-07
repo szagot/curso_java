369fd7b11ee11c3615a93a9ddf9c1e10fc1fc79e
Daniel Bispo <szagot@gmail.com>
2020-05-07 08:54:42 -0300

    Spring Boot: Projeto iniciado

---
+++ projeto_spring_boot_jpa/src/test/java/br/com/zefuinha/projeto_spring_boot_jpa/ProjetoSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zefuinha.projeto_spring_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
