682aa4bb969ff97c236ed4dca5ae8194bc9c4dee
Daniel Bispo <szagot@gmail.com>
2020-05-08 13:24:14 -0300

    JavaFX: Criando a primeira tela

--- projeto_javafx/src/application/Main.java
+++ projeto_javafx/src/application/Main.java
@@ -1,25 +1,32 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+
+			// Pega a viu criada no Scene Builder
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+
+			// Define como cena inicial
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+
+			// Inicia a cena
+			stage.show();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
