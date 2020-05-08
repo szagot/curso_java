5941c65e6b1a0fc0ab80eefc41feae8157fa7ed5
Daniel Bispo <szagot@gmail.com>
2020-05-08 15:36:04 -0300

    JavaFX: Criando combobox

--- projeto_javafx/src/gui/ViewController.java
+++ projeto_javafx/src/gui/ViewController.java
@@ -1,47 +1,48 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
 public class ViewController implements Initializable {
 
 	@FXML
-	private TextField txtNumber1;
-	@FXML
-	private TextField txtNumber2;
-	@FXML
-	private Label lblResult;
-	@FXML
-	private Button btSum;
+	private ComboBox<Person> comboBoxPerson;
+
+	// Implemetan o comportamento entre a minha lista e o combobox
+	private ObservableList<Person> obsList;
 
 	@FXML
-	public void onBtSumAction() {
+	private Button btAll;
 
-		try {
+	@FXML
+	public void onComboBoxPersonAction() {
+		// Pega o item selecionado
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
 
-			// Pega os textos das caixas
-			double number1 = Double.parseDouble(txtNumber1.getText().replace(',', '.'));
-			double number2 = Double.parseDouble(txtNumber2.getText().replace(',', '.'));
-			double sum = number1 + number2;
+		// Imprime o item selecionado no console
+		System.out.println(person);
+	}
 
-			// Mostra o resultado na label de resultado
-			lblResult.setText(String.format("%.2f", sum));
-
-		} catch (NumberFormatException e) {
-
-			Alerts.showAlert("Erro!", "Digite apenas números válidos", e.getMessage(), AlertType.ERROR);
-
+	@FXML
+	public void onBtAllAction() {
+		// Pega cada uma das pessoas
+		for(Person person:comboBoxPerson.getItems()) {
+			// E imprime no console
+			System.out.println(person);
 		}
-
 	}
 
 	/**
@@ -49,14 +50,30 @@ public class ViewController implements Initializable {
 	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		// Marca os campos do tipo double
-		Constraints.setTextFieldDouble(txtNumber1);
-		Constraints.setTextFieldDouble(txtNumber2);
-		
-		// Coloca um limite de caracteres
-		Constraints.setTextFieldMaxLength(txtNumber1, 10);
-		Constraints.setTextFieldMaxLength(txtNumber2, 10);
+
+		// Cria uma lista de pessoas para exemplo (seria puxado do BD)
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person(1, "Daniel Bispo", "szagot@gmail.com"));
+		persons.add(new Person(2, "Alini Bispo", "alini.r.bispo@gmail.com"));
+		persons.add(new Person(3, "Filipe Daniel", "filipe.d.bispo@gmail.com"));
+		persons.add(new Person(4, "Alejandro Gabriel", "alejandro.r.bispo@gmail.com"));
+
+		// Define um ObservableList a partir da lista
+		obsList = FXCollections.observableArrayList(persons);
+
+		// Adiciona a lista ao comboBox
+		comboBoxPerson.setItems(obsList);
+
+		// Define o padrão para pegar os nomes das pessoas
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
 
 	}
 
