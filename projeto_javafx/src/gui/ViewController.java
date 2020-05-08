7e1cc7ac82fa662b4aaa7e7a0d016f2a834767da
Daniel Bispo <szagot@gmail.com>
2020-05-08 14:31:48 -0300

    JavaFX: Alterando projeto para somar dois números

--- projeto_javafx/src/gui/ViewController.java
+++ projeto_javafx/src/gui/ViewController.java
@@ -3,18 +3,39 @@ package gui;
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
+	@FXML
+	private TextField txtNumber2;
+	@FXML
+	private Label lblResult;
+	@FXML
+	private Button btSum;
 
 	@FXML
-	public void onBtTestAction() {
+	public void onBtSumAction() {
 
-		// Mostra caixa de alerta
-		Alerts.showAlert("Que coisa!", null, "Você clicou mesmo, hein?!", AlertType.INFORMATION);
+		try {
+
+			// Pega os textos das caixas
+			double number1 = Double.parseDouble(txtNumber1.getText().replace(',', '.'));
+			double number2 = Double.parseDouble(txtNumber2.getText().replace(',', '.'));
+			double sum = number1 + number2;
+
+			// Mostra o resultado na label de resultado
+			lblResult.setText(String.format("%.2f", sum));
+
+		} catch (NumberFormatException e) {
+
+			Alerts.showAlert("Erro!", "Digite apenas números válidos", e.getMessage(), AlertType.ERROR);
+
+		}
 
 	}
 
