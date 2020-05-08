93f6d27cec662ae9974c13f342d8f671aa83eccf
Daniel Bispo <szagot@gmail.com>
2020-05-08 13:45:32 -0300

    JavaFX: Criando alerta para o botão btTest

--- projeto_javafx/src/gui/ViewController.java
+++ projeto_javafx/src/gui/ViewController.java
@@ -1,7 +1,9 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
@@ -10,7 +12,10 @@ public class ViewController {
 
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("Clicado!");
+
+		// Mostra caixa de alerta
+		Alerts.showAlert("Que coisa!", null, "Você clicou mesmo, hein?!", AlertType.INFORMATION);
+
 	}
 
 }
