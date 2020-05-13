8d2beeae74f79f40ec39cb6ccac30e70a665098a
Daniel Bispo <szagot@gmail.com>
2020-05-13 11:17:15 -0300

    Spring Boot MongoDB: Usando padrão DTO para retornar usuários

--- projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/resources/UserResource.java
+++ projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package br.com.zefuinha.projeto_spring_boot_mongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zefuinha.projeto_spring_boot_mongo.domain.User;
+import br.com.zefuinha.projeto_spring_boot_mongo.dto.UserDTO;
 import br.com.zefuinha.projeto_spring_boot_mongo.services.UserService;
 
 /**
@@ -25,10 +25,15 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
+
+		List<User> user = service.findAll();
+
+		// Converte para DTO
+		List<UserDTO> userDTO = user.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
 		// Resposta completa com headers e body
-		return ResponseEntity.ok().body(service.findAll());
+		return ResponseEntity.ok().body(userDTO);
 
 	}
 
