f22f5c42b2445e8e697d476e6cb2ea3705948bf3
Daniel Bispo <szagot@gmail.com>
2020-05-13 09:59:37 -0300

    Spring Boot MongoDB: Iniciado projeto

---
+++ projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_jpa/ProjetoSpringBootMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zefuinha.projeto_spring_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootMongoApplication.class, args);
+	}
+
+}
