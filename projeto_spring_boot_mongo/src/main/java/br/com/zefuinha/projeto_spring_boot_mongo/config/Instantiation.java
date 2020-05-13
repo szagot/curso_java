ad3c7e67b2598145b513a3abc4d20694a4ef2e49
Daniel Bispo <szagot@gmail.com>
2020-05-13 16:33:52 -0300

    Spring Boot MongoDB: Criando entity Post com User aninhado

--- projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/config/Instantiation.java
+++ projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package br.com.zefuinha.projeto_spring_boot_mongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.zefuinha.projeto_spring_boot_mongo.domain.Post;
 import br.com.zefuinha.projeto_spring_boot_mongo.domain.User;
+import br.com.zefuinha.projeto_spring_boot_mongo.repositories.PostRepository;
 import br.com.zefuinha.projeto_spring_boot_mongo.repositories.UserRepository;
 
 /**
@@ -18,19 +22,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-		// Apagando usuários existentes
-		userRepository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		// Instanciando dados iniciais
+		// Apagando dados existentes
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		// Salvando usuários de teste
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para são paulo, abraços!",
+				maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
