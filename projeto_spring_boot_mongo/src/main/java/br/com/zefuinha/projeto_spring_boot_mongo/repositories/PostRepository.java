499abf5ba09e9c84ae60af457af38e98c8352e9c
Daniel Bispo <szagot@gmail.com>
2020-05-14 11:00:19 -0300

    JavaFX JDBC: Consulta simples com @Query

--- projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/repositories/PostRepository.java
+++ projeto_spring_boot_mongo/src/main/java/br/com/zefuinha/projeto_spring_boot_mongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.zefuinha.projeto_spring_boot_mongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.zefuinha.projeto_spring_boot_mongo.domain.Post;
@@ -22,5 +23,22 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	 * @return
 	 */
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	/**
+	 * Query Personlizado -> {<field>: {$regex: /pattern/, $options: '<options>'}}
+	 * 
+	 * ?0 -> primeiro parâmetro do método
+	 * 
+	 * i -> ignora case
+	 * 
+	 * https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+	 * https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	 * https://docs.mongodb.com/manual/reference/operator/query/regex/
+	 * 
+	 * @param text
+	 * @return
+	 */
+	@Query(value = "{'text': {$regex : ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 
 }
